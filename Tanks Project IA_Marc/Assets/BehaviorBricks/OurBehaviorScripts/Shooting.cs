ece8ad5b3a2224b828a7364f4b5dab8b24c5d00d
M1R4B3L <amirabelmartinez@gmail.com>
2020-12-07 11:54:29 +0100

    Shooting 1M bullets

--- Tanks Project IA_Marc/Assets/BehaviorBricks/OurBehaviorScripts/Shooting.cs
+++ Tanks Project IA_Marc/Assets/BehaviorBricks/OurBehaviorScripts/Shooting.cs
@@ -4,70 +4,70 @@ using Pada1.BBCore;           // Code attributes
 using Pada1.BBCore.Tasks;     // TaskStatus
 using Pada1.BBCore.Framework; // BasePrimitiveAction
 
-[Action("OurBehaviorScripts/Shooting")]
-[Help("Clone a 'bullet' and shoots it through the Forward axis with the " +
-      "specified velocity.")]
-public class Shooting : BasePrimitiveAction
+namespace BBUnity.Actions
 {
-   
-    [InParam("shell")]
-    [Help("Bullet shoot")]
-    public Rigidbody m_Shell;
-
-    [InParam("spawnTransform")]
-    [Help("Starting position of the shell")]
-    public Transform m_FireTransform;
-
-    [InParam("minLaunchForce")]
-    public float m_MinLaunchForce;
-
-    [InParam("maxLaunchForce")]
-    public float m_MaxLaunchForce = 30f;
-
-    [InParam("currentLaunchForce")]
-    public float m_CurrentLaunchForce;
-
-    [InParam("currentTank")]
-    public GameObject CurrentTank;
-
-    [InParam("enemyTank")]
-    public GameObject EnemyTank;
-
-    [InParam("numBullet")]
-    bool bullet { get; set; }
-
-    private void OnEnable()
+    [Action("OurBehaviorScripts/Shooting")]
+    [Help("Clone a 'bullet' and shoots it through the Forward axis with the " +
+          "specified velocity.")]
+    public class Shooting : GOAction
     {
-        m_CurrentLaunchForce = m_MinLaunchForce;
-    }
 
-    public override void OnStart()
-    {
-        bullet = true;
-    }
+        [InParam("shell")]
+        [Help("Bullet shoot")]
+        public Rigidbody m_Shell;
+        public int numberBullets;
 
-    public override TaskStatus OnUpdate()
-    {
-      
-    
-        m_CurrentLaunchForce = 30;
+        [InParam("minLaunchForce")]
+        public float m_MinLaunchForce;
 
-        Fire();
+        [InParam("maxLaunchForce")]
+        public float m_MaxLaunchForce = 30f;
 
-        bullet = false;
+        [InParam("currentLaunchForce")]
+        public float m_CurrentLaunchForce;
 
-        return TaskStatus.RUNNING;
-    
-    }
+        [InParam("currentTank")]
+        public GameObject CurrentTank;
 
-    private void Fire()
-    {
-        Rigidbody shellInstance =
-            GameObject.Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
-        shellInstance.velocity = m_CurrentLaunchForce * (m_FireTransform.forward);
+        private Transform spawnShoot;
 
-        m_CurrentLaunchForce = m_MinLaunchForce;
-    }
+        private void OnEnable()
+        {
+            m_CurrentLaunchForce = m_MinLaunchForce;
+        }
 
-} // class Shooting
+        public override void OnStart()
+        {
+           if(spawnShoot == null)
+            {
+                Transform turret = CurrentTank.GetComponentInChildren<Transform>().Find("TankRenderers").Find("TankTurret").Find("FireTransform");
+
+                spawnShoot = turret;
+            }
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+
+            m_CurrentLaunchForce = 30;
+
+            Fire();
+
+            return TaskStatus.RUNNING;
+
+        }
+
+        private void Fire()
+        {
+            Rigidbody shellInstance =
+                GameObject.Instantiate(m_Shell, spawnShoot.position, spawnShoot.rotation) as Rigidbody;
+
+            shellInstance.velocity = m_CurrentLaunchForce * (spawnShoot.forward);
+
+            m_CurrentLaunchForce = m_MinLaunchForce;
+        }
+
+    } // class Shooting
+
+}
