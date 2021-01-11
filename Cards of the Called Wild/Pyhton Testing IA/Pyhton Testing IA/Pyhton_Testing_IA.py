581265b833a29a102ddcfca22397a7b5c9165f23
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2021-01-11 17:55:29 +0100

    pyhton

---
+++ Cards of the Called Wild/Pyhton Testing IA/Pyhton Testing IA/Pyhton_Testing_IA.py
@@ -0,0 +1,46 @@
+print("Hello, World!")
+
+
+
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+
+
+#Functions
+
+def Alo():
+
+	h=9
+	i=8
+
+	print(i,h)
+
+
+
+
+
+a=6
+
+
+
+Alo()	
+
+print(a)
+
+Alo()
+hola="hola bon dia"
+
+print(hola.swapcase())
+
+hello=hola+hola
+
+print(hello)
+b=9.9
+
+
+
+
